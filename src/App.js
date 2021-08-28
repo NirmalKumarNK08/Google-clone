b43a846df7e7ceaea05ba0042beae9bbd9642bb9
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2021-08-28 14:42:57 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>Hello</h1>
+          </Route>
+          <Route path="/home">
+            <h1>Here is what everything comes in.</h1>
+          </Route>
+        </Switch>
+
+        {/* Homepage with text-box or searchbox, buttons. */}
+        <Home />
+
+        {/* Homepage with text-box or searchbox, buttons. */}
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,25 @@
-import React from 'react';
 import './Home.css';
+import { Link } from "react-router-dom";
 
 function Home() {
    return (
-      <div className="home">
-         <div className="home_header">
-            <div className="header_left">
-               {/* Link */}
-               {/* Link */}
-            </div>
-            <div className="header_right">
-               {/* Link */}
-               {/* Link */}
-               {/* Icon */}
-               {/* Avatar */}
-            </div>
-         </div>
-         <div className="home_body">
-            
-         </div>
+     <div className="home">
+      <h1>I am here</h1>
+      <div className="home_header">
+      <div className="header_left">
+         <Link to="/about">About</Link>
+         <Link to="/store">Store</Link>
       </div>
-   )
+      <div className="header_right">
+         <Link to="/gmail">Gmail</Link>
+         <Link to="/images">Images</Link>
+         {/* Icon */}
+         {/*  */}
+      </div>
+      </div>
+      <div className="home_body"></div>
+     </div>
+   );
 }
 
 export default Home;
