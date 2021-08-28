e0d61874be2006f3ff159b4a4d0082ab11c3abe7
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2021-08-28 14:00:29 +0530

    Modified and added new filoes

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+   return (
+      <div className="home">
+         <div className="home_header">
+            <div className="header_left">
+               {/* Link */}
+               {/* Link */}
+            </div>
+            <div className="header_right">
+               {/* Link */}
+               {/* Link */}
+               {/* Icon */}
+               {/* Avatar */}
+            </div>
+         </div>
+         <div className="home_body">
+            
+         </div>
+      </div>
+   )
+}
+
+export default Home;
