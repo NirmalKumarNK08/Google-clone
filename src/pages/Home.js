3cdeb2ce961f3ca11376149fe652021e9fb0d8c3
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2021-09-01 15:22:44 +0530

    Uploading files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,32 @@
 import './Home.css';
 import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "../Search.js";
 
 function Home() {
    return (
      <div className="home">
-      <div className="home_header">
-      <div className="header_left">
-         <Link to="/about">About</Link>
-         <Link to="/store">Store</Link>
-      </div>
-      <div className="header_right">
-         <Link to="/gmail">Gmail</Link>
-         <Link to="/images">Images</Link>
-         {/* Icon */}
-         {/*  */}
-      </div>
-      </div>
-      <div className="home_body"></div>
+       <div className="home_header">
+         <div className="header_left">
+           <Link to="/about">About</Link>
+           <Link to="/store">Store</Link>
+         </div>
+         <div className="header_right">
+           <Link to="/gmail">Gmail</Link>
+           <Link to="/images">Images</Link>
+           <AppsIcon />
+           <Avatar />
+         </div>
+       </div>
+       <div className="home_body">
+         <img
+           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+           alt=""></img>
+         <div className="home_inputContainer">
+            <Search />
+         </div>
+       </div>
      </div>
    );
 }
